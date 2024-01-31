6153bcd00d96c37ef9f38f127f0ab54902bd1cfa
Your Name <you@example.com>
2024-01-31 19:04:33 +0200

    11-binary_tree_size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    size_t leftSize = 0;
+    size_t rightSize = 0;
+    if (tree == NULL) return (0);
+    if (tree->left == NULL && tree->right == NULL)
+        return (1);
+
+    leftSize = binary_tree_size(tree->left);
+    rightSize = binary_tree_size(tree->right);
+
+    return (leftSize + rightSize + 1);
+}
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,7 +8,8 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t leftHeight = 0, rightHeight = 0;
+    size_t leftHeight = 0;
+    size_t rightHeight = 0;
 
     if (tree == NULL)
         return (0);
