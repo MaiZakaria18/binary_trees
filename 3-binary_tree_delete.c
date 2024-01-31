101a619d965ef8596e92c113ea5c6707c5ef9a67
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-01-31 21:27:38 +0200

    betty

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,14 +7,14 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
+	if (tree == NULL)
+		return;
 
-    else
-    {
-        binary_tree_delete(tree->left);
-        binary_tree_delete(tree->right);
-    }
+	else
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+	}
 
-    free(tree);
+	free(tree);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,19 +8,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t leftHeight = 0;
-    size_t rightHeight = 0;
+	size_t leftHeight = 0;
+	size_t rightHeight = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left)
-        leftHeight = binary_tree_height(tree->left) + 1;
-    if (tree->right)
-        rightHeight = binary_tree_height(tree->right) + 1;
+	if (tree->left)
+		leftHeight = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		rightHeight = binary_tree_height(tree->right) + 1;
 
-    if (leftHeight > rightHeight)
-        return (leftHeight);
-    else
-        return (rightHeight);
+	if (leftHeight > rightHeight)
+		return (leftHeight);
+	else
+		return (rightHeight);
 }
