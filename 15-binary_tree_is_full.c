bf4f51879e455bf59a009d4604c21c242e16d156
Your Name <you@example.com>
2024-01-31 22:07:40 +0200

    binary_tree_is_full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -14,7 +14,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-
 	if (tree->left == NULL && tree->right != NULL)
 		return (0);
 
--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,10 +1,16 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_sibling - Finds the sibling of a node in a binary tree
+ * @node: Pointer to the node to find the sibling
+ *
+ * Return: Pointer to the sibling node, or NULL if no sibling exists
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    binary_tree_t *sibling;
+    binary_tree_t *sibling = NULL;
 
-    if (node == NULL ||  node->parent == NULL)
+    if (node == NULL || node->parent == NULL)
         return (NULL);
 
     if (node->parent->left == node)
