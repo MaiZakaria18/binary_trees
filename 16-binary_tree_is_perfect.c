83ae5d2b2e861a4f4b3151c5ec2d2f4ec048acd8
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-01-31 22:57:54 +0200

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,25 @@
 #include "binary_trees.h"
-
+/**
+ * max - that calculate the max value
+ * @a: first value
+ * @b: second value
+ * Return: the max value
+*/
+int max(int a, int b)
+{
+	return (a > b ? a : b);
+}
+/**
+ * height - get height of binary tree
+ * @tree: pointer to the nood
+ * Return: the height
+*/
+int height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (1 + max(height(tree->left), height(tree->right)));
+}
 /**
  * binary_tree_is_perfect - function that checks if a binary tree is perfect
  * @tree:  is a pointer to the root node of the tree to check
@@ -7,23 +27,14 @@
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t leftDepth;
-	size_t rightDepth;
-	
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	leftDepth = binary_tree_is_perfect(tree->left);
-	rightDepth = binary_tree_is_perfect(tree->right);
-
-	if (leftDepth > rightDepth)
+	if ((height(tree->left) == height(tree->right)) &&
+	(tree->left != NULL && tree->right != NULL))
 	{
-		return (leftDepth);
+		return (1);
 	}
-	else
-	{
-		return (rightDepth);
-	}
+	return (0);
 }
