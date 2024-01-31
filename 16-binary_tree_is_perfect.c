ea596b326db3521215b716af6b8fb8e85d5d4934
Your Name <you@example.com>
2024-01-31 23:27:07 +0200

    test

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -31,14 +31,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
  *
  * Return: 1 if the tree is perfect, 0 otherwise
  */
-#include "binary_trees.h"
-
-/**
- * binary_tree_is_perfect - Checks if a binary tree is perfect
- * @tree: Pointer to the root node of the tree
- *
- * Return: 1 if the tree is perfect, 0 otherwise
- */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
